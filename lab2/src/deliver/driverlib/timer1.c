98fcebbdf633aaa18d3dacfe06f7eb5277858476
luke-jiang <yuxuanlukejiang@gmail.com>
2018-07-06 03:05:17 -0700

    add comment

--- lab2/src/deliver/driverlib/timer1.c
+++ lab2/src/deliver/driverlib/timer1.c
@@ -2,16 +2,14 @@
 
 void timer1_setup(uint32_t speed, int interrupt) {
   RCGCTIMER |= 0x001;
-
   T1_CTL = TAEN_OFF;    // disable timer while configuring
   T1_CFG = 0x00;        // select 32-bit timer configuration
   T1_TAMR = 0x02;       // select countdown, periodic mode
   T1_TAILR = speed;     // set speed
-
   if (interrupt) {
-    T1_MIMR = 0x01;
-    EN1 |= (1 << 21);
-    T1_ICR |= 0x01;
+    T1_MIMR = 0x01;     // enable interrupt mask
+    EN1 |= (1 << 21);   // enable timer1 at NVIC
+    T1_ICR |= 0x01;     // clear timer
   }
 }
 

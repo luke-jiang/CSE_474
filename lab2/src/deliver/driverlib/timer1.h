7ad26a8e3400b493d5abe9a8d90fe8b7293ad1bc
luke-jiang <yuxuanlukejiang@gmail.com>
2018-07-06 03:05:41 -0700

    add comment

--- lab2/src/deliver/driverlib/timer1.h
+++ lab2/src/deliver/driverlib/timer1.h
@@ -1,6 +1,7 @@
 #ifndef TIMER1_H
 #define TIMER1_H
 
+// Registers configuring timer1
 #define RCGCTIMER         (*((volatile uint32_t *) 0x400FE604))
 #define EN0               (*((volatile uint32_t *) 0xE000E100))
 #define T1_CTL            (*((volatile uint32_t *) 0x4003100C))
@@ -11,15 +12,21 @@
 #define T1_ICR            (*((volatile uint32_t *) 0x40031024))
 #define T1_MIMR           (*((volatile uint32_t *) 0x40031018))
 
-#define SEC1      0x00F42400
-#define SEC2      0x01E84800
-#define SEC5      0x04C4B400
-#define TAEN_OFF  0x00
-#define TAEN_ON   0x01
+// Inputs to T0_TAILR for different speeds
+#define SEC1              0x00F42400  // 1 second
+#define SEC2              0x01E84800  // 2 seconds
+#define SEC5              0x04C4B400  // 5 seconds
 
+#define TAEN_OFF          0x00
+#define TAEN_ON           0x01
+
+// Set up timer1 with given speed; allow interrupt if interrupt == 1
 void timer1_setup(uint32_t speed, int interrupt);
+// Enable timer1 if on == 1, otherwise disable timer1
 void timer1_ctrl(int on);
+// Return 1 when time is out; else return 0
 int timer1_out();
+// Clear the timer and begin next period
 void timer1_clear();
 
 #endif
